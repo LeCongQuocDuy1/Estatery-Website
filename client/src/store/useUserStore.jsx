9ccd6d9ac912743e35e45af518653cd432cbf20a
NEWUSER <duy9117@gmai.com>
2023-11-29 18:28:57 +0700

    config zustand persist middleware and create localstorage

--- client/src/store/useUserStore.jsx
+++ client/src/store/useUserStore.jsx
@@ -1,6 +1,22 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-export const useUserStore = create(() => ({
-    token: null,
-    current: null,
-}));
+export const useUserStore = create(
+    persist(
+        (set, get) => ({
+            token: null,
+            current: null,
+        }),
+        {
+            name: "estatery",
+            storage: createJSONStorage(() => localStorage),
+            // return 1 cái object của các state muốn lưu
+            partialize: (state) =>
+                Object.fromEntries(
+                    Object.entries(state).filter(
+                        (el) => el[0] === "token" || el[0] === "current"
+                    )
+                ),
+        }
+    )
+);
